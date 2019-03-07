e93aaf53aa893171fc3e200fe204aa8b13435c00
SebiTCR <Ssebi832@outlook.com>
2019-03-07 15:05:00 +0200

    2

---
+++ Experiments/Phaser/script/config.js
@@ -0,0 +1,15 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
